69e76eb026e5f49b2da1350f1dc08f2fad1e61a1
Yoofi Quansah <ybquansah@gmail.com>
2024-10-30 15:02:31 -0500

    chore: only create namespaces on bash script (#37)
    * chore: only create namespaces on bash script
    
    * chore: modify bash script to wait for argo components
    
    * chore: modify script a little more

--- install.bash
+++ install.bash
@@ -21,11 +21,43 @@ sudo microk8s enable argocd
 sudo microk8s enable nvidia
 
 sudo microk8s kubectl create namespace hyperdos
+sudo microk8s kubectl create namespace hyperweb
+sudo microk8s kubectl create namespace instance 
+sudo microk8s kubectl create namespace ping
+
 sudo microk8s helm repo add hyperdos https://hyperboliclabs.github.io/Hyper-dOS
 
 echo "Starting hyperdos software..."
 if [[ "$DEV" == "true" ]]; then
   echo "Install in dev mode..."
 fi
+
+namespace="argocd"
+
+echo "Waiting for $namespace components to be ready..."
+
+while true; do
+  pods=$(sudo microk8s kubectl get pods -n "$namespace" --no-headers 2>&1)
+
+  if [ "$pods" == "No resources found in $namespace namespace." ]; then
+    echo "All $namespace components not ready yet."
+    sleep 5
+    continue
+  fi
+
+  # Get the number of non-ready pods
+  not_ready_pods=$(echo $pods | awk '$3 != "Running" && $3 != "Completed" {print $1}' | wc -l)
+
+  if [ "$not_ready_pods" -eq 0 ]; then
+    echo "All $namespace components are ready!"
+    break
+  else
+    echo "$not_ready_pods $namespace components are not ready yet. Checking again in 10 seconds..."
+    sleep 10
+  fi
+done
+
+echo "Installing hyperdos now..."
+sleep 20 
 
 sudo microk8s helm install hyperdos hyperdos/hyperdos --version 0.0.1-alpha.4 --set token=$TOKEN $EXTRA_PARAMS
