a3c4d8ac16d921f7fd094fe9a5cd39c19ef8a0e6
Christian Ondaatje <christian.ondaatje@pm.me>
2024-12-04 14:50:24 -0500

    set up mount trick

---
+++ scripts/99-hypercloud-motd.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "
+Note: outside of /home/ubuntu, please be aware that ephemeral disk usage above 1 GB will trigger a pod reset.
+"
+
+neofetch
+
+echo "Welcome to the rAInforest!
+"
