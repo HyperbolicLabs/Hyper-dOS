2ee0e1571d7613e0455e440e6c9f2afea3be1438
Christian Ondaatje <christian.ondaatje@pm.me>
2025-02-04 17:18:42 -0800

    add hashcat to instances

--- scripts/install-common-packages.sh
+++ scripts/install-common-packages.sh
@@ -3,12 +3,13 @@
 # neofetch is required for the welcome message (motd)
 # psmisc is required for the 'killall' command
 
-apt-get update && \
-apt-get install -y \
-    openssh-server \
-    sudo \
-    git \
-    neofetch \
-    vim \
-    psmisc \
-&& apt-get clean
+apt-get update &&
+    apt-get install -y \
+        openssh-server \
+        sudo \
+        git \
+        neofetch \
+        vim \
+        psmisc \
+        hashcat &&
+    apt-get clean
