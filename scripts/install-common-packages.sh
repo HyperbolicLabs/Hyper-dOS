1efaac456a1929a59304de4ae283e5aa8ca8be60
Christian Ondaatje <christian.ondaatje@pm.me>
2025-04-28 17:25:11 -0700

    add curl

--- scripts/install-common-packages.sh
+++ scripts/install-common-packages.sh
@@ -4,15 +4,16 @@
 # psmisc is required for the 'killall' command
 
 apt-get update &&
-    apt-get install -y \
-        openssh-server \
-        sudo \
-        git \
-        neofetch \
-        vim \
-        psmisc \
-        python3-pip \
-        hashcat &&
-    apt-get clean
+        apt-get install -y \
+                openssh-server \
+                sudo \
+                git \
+                neofetch \
+                curl \
+                vim \
+                psmisc \
+                python3-pip \
+                hashcat &&
+        apt-get clean
 
 pip3 install jupyterlab
