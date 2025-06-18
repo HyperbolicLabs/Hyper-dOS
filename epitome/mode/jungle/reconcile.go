e1d07e765327e7cd1ce801ee8241c094df04ef44
Christian Ondaatje <christian.ondaatje@pm.me>
2025-06-18 10:18:30 -0700

    improve logging

--- epitome/mode/jungle/reconcile.go
+++ epitome/mode/jungle/reconcile.go
@@ -4,17 +4,19 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/sirupsen/logrus"
+	"go.uber.org/zap"
 )
 
 func (a *agent) reconcile() error {
 	if !secretExists(a.clientset, a.cfg.HyperwebNamespace, "operator-oauth") {
 		// if it does not, query the gateway for oauth credentials using our token
-		logrus.Infof("operator-oauth secret does not exist in namespace: %v", a.cfg.HyperwebNamespace)
+		a.logger.Info(
+			"operator-oauth secret does not exist",
+			zap.String("namespace", a.cfg.HyperwebNamespace))
 
 		response, err := a.handshake(http.DefaultClient)
 		if err != nil {
-			logrus.Errorf("failed to handshake with gateway: %v", err)
+			a.logger.Error("failed to handshake with gateway", zap.Error(err))
 			return err
 		}
 
@@ -25,39 +27,37 @@ func (a *agent) reconcile() error {
 
 		err = a.installClusterNameConfigMap(response.ClusterName)
 		if err != nil {
-			logrus.Errorf("failed to save cluster name in configmap: %v", err)
+			a.logger.Error("failed to save cluster name in configmap: %v", zap.Error(err))
 			return err
 		}
 	}
 
 	name, err := a.getClusterName()
 	if err != nil {
-		logrus.Errorf("failed to get cluster name: %v", err)
+		a.logger.Error("failed to get cluster name: %v", zap.Error(err))
 		return err
 	}
 
 	if a.HyperwebIsInstalled() {
 		if a.isRegistered() {
-			logrus.Infof("hyperweb application is installed and registered, nothing to do")
+			a.logger.Debug("hyperweb application is installed and registered, nothing to do")
 		} else {
-			response, err := a.register(
-				*name,
-			)
+			response, err := a.register(*name)
 			if err != nil {
 				return err
 			}
 			if response.Success {
-				logrus.Infof("registered cluster %s with gateway", *name)
+				a.logger.Info("registered cluster with gateway", zap.String("cluster name", *name))
 			} else {
 				return fmt.Errorf("failed to register cluster %s with gateway", *name)
 			}
 		}
 	} else {
-		logrus.Infof("hyperweb application is not installed - installing now")
+		a.logger.Info("hyperweb application is not installed - installing now")
 
 		err = a.installHyperWeb(*name)
 		if err != nil {
-			logrus.Errorf("failed to install hyperweb application: %v", err)
+			a.logger.Error("failed to install hyperweb application", zap.Error(err))
 			return err
 		}
 	}
